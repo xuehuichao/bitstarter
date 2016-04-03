cb8f64852aafa1ad14a96c842af9c49820d322d1
Huichao Xue <xue.huichao@gmail.com>
2016-04-03 19:15:30 +0000

    :

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
+  var content = fs.readFileSync('index.html', 'utf8');
+  
+  response.send(content);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
